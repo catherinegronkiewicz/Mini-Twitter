6bef06f93ae9811ad642bc37aee9823c7ca7a5f1
Catherine Gronkiewicz <35318088+catherinegronkiewicz@users.noreply.github.com>
2020-11-11 22:20:37 -0800

    Update User.java

--- src/miniTwitter/User.java
+++ src/miniTwitter/User.java
@@ -15,70 +15,70 @@ import java.util.HashMap;
 
 public class User implements UserElement, Observer, Subject {
 
-	private String uniqueID;
-	private HashMap<String, User> userMap = AdminPanel.totalUserMap();
-	private ArrayList<String> userFollowers;
-	private ArrayList<String> userFollowings;
-	private ArrayList<String> userFeed;
+    private String uniqueID;
+    private HashMap<String, User> userMap = AdminPanel.totalUserMap();
+    private ArrayList<String> userFollowers;
+    private ArrayList<String> userFollowings;
+    private ArrayList<String> userFeed;
 
-	public User(String twitterUser) {
-		uniqueID = twitterUser;
-		userFollowings = new ArrayList<String>();
-		userFollowers = new ArrayList<String>();
-		userFeed = new ArrayList<String>();
-	}
+    public User(String twitterUser) {
+	uniqueID = twitterUser;
+	userFollowings = new ArrayList<String>();
+	userFollowers = new ArrayList<String>();
+	userFeed = new ArrayList<String>();
+    }
 	
-	public User getUser() {
-		return User.this;
-	}
+    public User getUser() {
+	return User.this;
+    }
 	
-	public ArrayList<String> getUserFollowers() {
-		return userFollowers;
-	}
+    public ArrayList<String> getUserFollowers() {
+	return userFollowers;
+    }
 
-	public ArrayList<String> getUserFollowings() {
-		return userFollowings;
-	}
+    public ArrayList<String> getUserFollowings() {
+	return userFollowings;
+    }
 
-	public ArrayList<String> getUserFeed() {
-		return userFeed;
-	}
+    public ArrayList<String> getUserFeed() {
+	return userFeed;
+    }
 
-	// add methods - Composite pattern
-	public void addFollowings(String following) {
-		userFollowings.add(following);
-	}
+    // add methods - Composite pattern
+    public void addFollowings(String following) {
+	userFollowings.add(following);
+    }
 
-	// add methods - Composite pattern
-	public void addFollowers(String follower) {
-		userFollowers.add(follower);
-	}
+    // add methods - Composite pattern
+    public void addFollowers(String follower) {
+	userFollowers.add(follower);
+    }
 
-	// update followers with new tweets - Observer pattern
-	@Override
-	public void update(String tweet) {
-		userFeed.add(tweet);
-	}
+    // update followers with new tweets - Observer pattern
+    @Override
+    public void update(String tweet) {
+	userFeed.add(tweet);
+    }
 
-	@Override
-	public String getUserID() {
-		return uniqueID;
-	}
+    @Override
+    public String getUserID() {
+	return uniqueID;
+    }
 
-	@Override
-	public void attach(String userName) {
-		getUser().addFollowings(userName);
-		// grabbing the following and updating followers list
-		userMap.get(userName).addFollowers(getUser().getUserID());
-	}
+    @Override
+    public void attach(String userName) {
+	getUser().addFollowings(userName);
+	// grabbing the following and updating followers list
+	userMap.get(userName).addFollowers(getUser().getUserID());
+    }
 
-	@Override
-	public void notifyObservers(String userTweet) {
-		int followerCount = getUser().getUserFollowers().size();
-		for (int i = 0; i < followerCount; i++) {
-			String twitterFollower = getUser().getUserFollowers().get(i);
-			// updates each follower with new tweet
-			userMap.get(twitterFollower).update(userTweet);
-		}
+    @Override
+    public void notifyObservers(String userTweet) {
+	int followerCount = getUser().getUserFollowers().size();
+	for (int i = 0; i < followerCount; i++) {
+	    String twitterFollower = getUser().getUserFollowers().get(i);
+	    // updates each follower with new tweet
+	    userMap.get(twitterFollower).update(userTweet);
 	}
+    }
 }
