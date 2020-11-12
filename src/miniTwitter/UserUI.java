f07230a0dca2b8d1634080dc3be4890e6de908e3
Catherine Gronkiewicz <35318088+catherinegronkiewicz@users.noreply.github.com>
2020-11-11 22:47:32 -0800

    Update UserUI.java

--- src/miniTwitter/UserUI.java
+++ src/miniTwitter/UserUI.java
@@ -1,5 +1,17 @@
 package miniTwitter;
 
+/*
+ * Catherine Gronkiewicz
+ * Professor Sun
+ * CS 3560, Section 01
+ * 12 November 2020
+ * 
+ * Client - runs Composite pattern components;
+ * creates user GUI and UI buttons to update 
+ * followers (of new tweets), post tweets, 
+ * and display followings list
+ */
+
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,171 +30,160 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
-/*
- * Catherine Gronkiewicz
- * Professor Sun
- * CS 3560, Section 01
- * 12 November 2020
- * 
- * Client - runs Composite pattern components;
- * creates user GUI and UI buttons to update 
- * followers (of new tweets), post tweets, 
- * and display followings list
- */
-
 @SuppressWarnings("serial")
 public class UserUI extends JFrame {
 	
-	private User currentUser;
-	private HashMap<String, User> mapOfUsers = AdminPanel.totalUserMap();
-	private JPanel contentPane;
-	private JScrollPane followingScrollPane, newsfeedScrollPane;
-	private JTextField followUserTextField, postTweetField;
-	private JButton followUserButton;
-	private JButton postTweetButton;
-	private JLabel currentlyFollowingLabel, userFeedLabel;
-	private ArrayList<String> listOfFollowings;
-	private DefaultListModel<String> followingListModel, userFeedListModel;
-	private JList<String> followingList, userFeedList;
-	private String userTweet, userToFollow;
-	private List<String> userFeed;
+    private User currentUser;
+    private HashMap<String, User> mapOfUsers = AdminPanel.totalUserMap();
+    private JPanel contentPane;
+    private JScrollPane followingScrollPane, newsfeedScrollPane;
+    private JTextField followUserTextField, postTweetField;
+    private JButton followUserButton;
+    private JButton postTweetButton;
+    private JLabel currentlyFollowingLabel, userFeedLabel;
+    private ArrayList<String> listOfFollowings;
+    private DefaultListModel<String> followingListModel, userFeedListModel;
+    private JList<String> followingList, userFeedList;
+    private String userTweet, userToFollow;
+    private List<String> userFeed;
 	
+    @SuppressWarnings("unchecked")
+    public UserUI(User user) {		
+		
+	// initializes the User "leaf" - Composite Pattern
+	currentUser = user.getUser();
+		
+	// generates User UI components
+	setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	setBounds(200, 200, 440, 480);
 
-	@SuppressWarnings("unchecked")
-	public UserUI(User user) {		
+	setTitle("Welcome to @" + currentUser.getUserID() + "'s Twitter!");
+	contentPane = new JPanel();
+	contentPane.setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
+	setContentPane(contentPane);
+	contentPane.setLayout(null);
+	contentPane.setBackground(Color.lightGray);
 		
-		// initializes the User "leaf" - Composite Pattern
-		currentUser = user.getUser();
+	userFeedListModel = new DefaultListModel<String>();
+	followingListModel = new DefaultListModel<String>();
 		
-		// generates User UI components
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(200, 200, 440, 480);
+	followUserButton = new JButton("Follow User");
+	followUserButton.setBounds(217, 12, 210, 23);
+	followUserButton.addActionListener(new UserUIButtons());
+	contentPane.add(followUserButton);
+		
+	currentlyFollowingLabel = new JLabel("Currently Following:");
+	currentlyFollowingLabel.setBounds(10, 45, 175, 15);
+	contentPane.add(currentlyFollowingLabel);
+		
+	followingList = new JList<String>(followingListModel);
+	followingList.setBounds(10, 65, 415, 145);
+	contentPane.add(followingList);
+	listOfFollowings = (ArrayList<String>) currentUser.getUserFollowings().clone();
+		
+	followUserTextField = new JTextField();
+	followUserTextField.setBounds(10, 13, 200, 18);
+	contentPane.add(followUserTextField);
+	followUserTextField.setBorder(BorderFactory.createLineBorder(Color.darkGray, 2));
 
-		setTitle("Welcome to @" + currentUser.getUserID() + "'s Twitter!");
-		contentPane = new JPanel();
-		contentPane.setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		contentPane.setBackground(Color.lightGray);
+	postTweetField = new JTextField();
+	postTweetField.setBounds(10, 235, 270, 20);
+	contentPane.add(postTweetField);
+	postTweetField.setBorder(BorderFactory.createLineBorder(Color.darkGray, 2));
 		
-		userFeedListModel = new DefaultListModel<String>();
-		followingListModel = new DefaultListModel<String>();
-		
-		followUserButton = new JButton("Follow User");
-		followUserButton.setBounds(217, 12, 210, 23);
-		followUserButton.addActionListener(new UserUIButtons());
-		contentPane.add(followUserButton);
-		
-		currentlyFollowingLabel = new JLabel("Currently Following:");
-		currentlyFollowingLabel.setBounds(10, 45, 175, 15);
-		contentPane.add(currentlyFollowingLabel);
-		
-		followingList = new JList<String>(followingListModel);
-		followingList.setBounds(10, 65, 415, 145);
-		contentPane.add(followingList);
-		listOfFollowings = (ArrayList<String>) currentUser.getUserFollowings().clone();
-		
-		followUserTextField = new JTextField();
-		followUserTextField.setBounds(10, 13, 200, 18);
-		contentPane.add(followUserTextField);
-		followUserTextField.setBorder(BorderFactory.createLineBorder(Color.darkGray, 2));
+	postTweetButton = new JButton("Post Tweet");
+	postTweetButton.setBounds(290, 234, 142, 25);
+	postTweetButton.addActionListener(new TweetButtons());
+	contentPane.add(postTweetButton);
 
-		postTweetField = new JTextField();
-		postTweetField.setBounds(10, 235, 270, 20);
-		contentPane.add(postTweetField);
-		postTweetField.setBorder(BorderFactory.createLineBorder(Color.darkGray, 2));
-		
-		postTweetButton = new JButton("Post Tweet");
-		postTweetButton.setBounds(290, 234, 142, 25);
-		postTweetButton.addActionListener(new TweetButtons());
-		contentPane.add(postTweetButton);
+	userFeedLabel = new JLabel("News Feed:");
+	userFeedLabel.setBounds(10, 270, 210, 15);
+	contentPane.add(userFeedLabel);
 
-		userFeedLabel = new JLabel("News Feed:");
-		userFeedLabel.setBounds(10, 270, 210, 15);
-		contentPane.add(userFeedLabel);
-
-		userFeedList = new JList<String>(userFeedListModel);
-		userFeedList.setBounds(10, 290, 415, 150);
-		userFeed = (ArrayList<String>) currentUser.getUserFeed().clone();
-		contentPane.add(userFeedList);
+	userFeedList = new JList<String>(userFeedListModel);
+	userFeedList.setBounds(10, 290, 415, 150);
+	userFeed = (ArrayList<String>) currentUser.getUserFeed().clone();
+	contentPane.add(userFeedList);
 		
-		// scroll pane for userfeed and following list
-		scrollRefresh();
+	// scroll pane for userfeed and following list
+	scrollRefresh();
 		
-		// add the user tweets to the userfeed
-		int userTweets = userFeed.size();
-		for (int i = 0; i < userTweets; i++){
-			userFeedListModel.addElement(userFeed.get(i));
-		}
-		
-		// add the user followings to the list
-		int followingsList = listOfFollowings.size();
-		for (int i = 0; i < followingsList; i++){
-			followingListModel.addElement(listOfFollowings.get(i));
-		}
+	// add the user tweets to the userfeed
+	int userTweets = userFeed.size();
+	for (int i = 0; i < userTweets; i++) {
+	    userFeedListModel.addElement(userFeed.get(i));
 	}
-	
-	public void scrollRefresh() {
 		
-		followingScrollPane = new JScrollPane(followingList);
-		followingScrollPane.setBounds(10, 68, 415, 150);
-		followingScrollPane.setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
-		contentPane.add(followingScrollPane);
-
-		newsfeedScrollPane = new JScrollPane(userFeedList);
-		newsfeedScrollPane.setBounds(10, 290, 415, 150);
-		newsfeedScrollPane.setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
-		contentPane.add(newsfeedScrollPane);
+	// add the user followings to the list
+	int followingsList = listOfFollowings.size();
+	for (int i = 0; i < followingsList; i++) {
+	    followingListModel.addElement(listOfFollowings.get(i));
 	}
+    }
 	
-	// posts tweets (to user's feed and their followers' feeds)
-	private class TweetButtons implements ActionListener {
+    public void scrollRefresh() {
+		
+	followingScrollPane = new JScrollPane(followingList);
+	followingScrollPane.setBounds(10, 68, 415, 150);
+	followingScrollPane.setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
+	contentPane.add(followingScrollPane);
+
+	newsfeedScrollPane = new JScrollPane(userFeedList);
+	newsfeedScrollPane.setBounds(10, 290, 415, 150);
+	newsfeedScrollPane.setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
+	contentPane.add(newsfeedScrollPane);
+	    
+    }
+	
+    // posts tweets (to user's feed and their followers' feeds)
+    private class TweetButtons implements ActionListener {
 				
-		@Override
-		public void actionPerformed(ActionEvent e) {	
+        @Override
+	public void actionPerformed(ActionEvent e) {	
 			
-			userTweet = "-   " + currentUser.getUserID() + ": " + postTweetField.getText();
+	    userTweet = "-   " + currentUser.getUserID() + ": " + postTweetField.getText();
 				
-			// current user's tweets are updated to followers
-			currentUser.notifyObservers(userTweet);
-			currentUser.update(userTweet);
-			userFeedListModel.addElement(userTweet);;
+	    // current user's tweets are updated to followers
+	    currentUser.notifyObservers(userTweet);
+	    currentUser.update(userTweet);
+	    userFeedListModel.addElement(userTweet);;
 				
-			// visits TotalTweetsVisitor class 
-			TotalTweetsVisitor tweetTotal = new TotalTweetsVisitor();
-			tweetTotal.accept(new DisplayVisitor());
+	    // visits TotalTweetsVisitor class 
+	    TotalTweetsVisitor tweetTotal = new TotalTweetsVisitor();
+	    tweetTotal.accept(new DisplayVisitor());
 				
-			// visits PositivePercentVisitor class
-			PositivePercentVisitor positiveTweets = new PositivePercentVisitor();
-			positiveTweets.accept(new DisplayVisitor());
-			positiveTweets.countPositiveTweets(userTweet);
+	    // visits PositivePercentVisitor class
+	    PositivePercentVisitor positiveTweets = new PositivePercentVisitor();
+	    positiveTweets.accept(new DisplayVisitor());
+	    positiveTweets.countPositiveTweets(userTweet);
 							
-		}
-	}	
-	
-	// follows users (with edge cases)
-	private class UserUIButtons implements ActionListener {
-		
-		@Override
-		public void actionPerformed(ActionEvent e) {	
-			
-			userToFollow = followUserTextField.getText();
-			
-			if (!mapOfUsers.containsKey(userToFollow)) {
-				JOptionPane.showMessageDialog(null, "User \"" + userToFollow + "\" does not exist!");
-				
-			} else if(currentUser.getUserFollowings().contains(userToFollow)){
-				JOptionPane.showMessageDialog(null, "You already follow the user \"" + userToFollow + "\"!");
-				
-			} else if (userToFollow.equals(currentUser.getUserID())) {
-				JOptionPane.showMessageDialog(null, "You cannot follow yourself!");
-				
-			} else {
-				// attach new follower to current user
-				currentUser.attach(userToFollow);
-				JOptionPane.showMessageDialog(null, "You are now following " + userToFollow + "!");
-				followingListModel.addElement(userToFollow);
-			}
-		}
 	}
+    }	
+	
+    // follows users (with edge cases)
+    private class UserUIButtons implements ActionListener {
+		
+	@Override
+	public void actionPerformed(ActionEvent e) {	
+			
+	    userToFollow = followUserTextField.getText();
+			
+	    if (!mapOfUsers.containsKey(userToFollow)) {
+		JOptionPane.showMessageDialog(null, "User \"" + userToFollow + "\" does not exist!");
+				
+	    } else if(currentUser.getUserFollowings().contains(userToFollow)){
+		JOptionPane.showMessageDialog(null, "You already follow the user \"" + userToFollow + "\"!");
+				
+	    } else if (userToFollow.equals(currentUser.getUserID())) {
+		JOptionPane.showMessageDialog(null, "You cannot follow yourself!");
+				
+	    } else {
+		// attach new follower to current user
+		currentUser.attach(userToFollow);
+		JOptionPane.showMessageDialog(null, "You are now following " + userToFollow + "!");
+		followingListModel.addElement(userToFollow);
+	    }
+	}
+    }
+	
 }
