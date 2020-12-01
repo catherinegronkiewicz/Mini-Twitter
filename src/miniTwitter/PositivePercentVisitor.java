ca41e99afbbd0839fe8a1b74af0cfed5796d48b1
Catherine Gronkiewicz <35318088+catherinegronkiewicz@users.noreply.github.com>
2020-12-01 15:36:51 -0800

    Update PositivePercentVisitor.java

--- src/miniTwitter/PositivePercentVisitor.java
+++ src/miniTwitter/PositivePercentVisitor.java
@@ -12,50 +12,50 @@ package miniTwitter;
 
 public class PositivePercentVisitor implements Visitable {
 	
-	// getting total tweets from TotalTweetsVisitor class
-	private TotalTweetsVisitor ttv = new TotalTweetsVisitor();
+    // getting total tweets from TotalTweetsVisitor class
+    private TotalTweetsVisitor ttv = new TotalTweetsVisitor();
 
-	public double totalTweets;
-	public static int posTotal;
-	public double positiveTweets;
-	double posPercent = 0.0;
+    public double totalTweets;
+    public static int posTotal;
+    public double positiveTweets;
+    double posPercent = 0.0;
 	
-	// positive words/tweets
-	String[] positiveWords = {"woohoo", "congrats", "awesome", "cool", "nice", "love", "excited"};
+    // positive words/tweets
+    String[] positiveWords = {"woohoo", "congrats", "awesome", "cool", "nice", "love", "excited"};
 	
-	public PositivePercentVisitor() {}
+    public PositivePercentVisitor() {}
 	
-	// counts total positive tweets
-	public void countPositiveTweets(String tweets) {
-		for (int i = 0; i < positiveWords.length; i++) {
-			if (tweets.contains(positiveWords[i])) {
-				posTotal++;
-			}
-		}
+    // counts total positive tweets
+    public void countPositiveTweets(String tweets) {
+	for (int i = 0; i < positiveWords.length; i++) {
+	    if (tweets.contains(positiveWords[i])) {
+		posTotal++;
+	    }
 	}
+    }
 	
-	// returns number of positive tweets
-	public int getPosTotal() {
-		return posTotal;
-	}
+    // returns number of positive tweets
+    public int getPosTotal() {
+	return posTotal;
+    }
 	
-	// method to calculate positive tweets amongst total tweets
-	public void calcPosPercent() {
-		totalTweets = (double)ttv.getTotalTweets();
-		positiveTweets = (double)getPosTotal();
-		posPercent = (positiveTweets/totalTweets) * 100.0;
-	}
+    // method to calculate positive tweets amongst total tweets
+    public void calcPosPercent() {
+	totalTweets = (double)ttv.getTotalTweets();
+	positiveTweets = (double)getPosTotal();
+	posPercent = (positiveTweets/totalTweets) * 100.0;
+    }
 	
-	// returns percentage of positive tweets
-	public double getPosPercent() {
-		calcPosPercent();
-		return posPercent;
-	}
+    // returns percentage of positive tweets
+    public double getPosPercent() {
+	calcPosPercent();
+	return posPercent;
+    }
 	
-	// accepts a PositivePercentVisitor visitor
-	@Override
-	public void accept(Visitor visit) {
-		visit.visit(this);		
-	}
+    // accepts a PositivePercentVisitor visitor
+    @Override
+    public void accept(Visitor visit) {
+	visit.visit(this);		
+    }
 
 }
