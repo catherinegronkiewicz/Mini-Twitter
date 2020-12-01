22e1dd441b7c1fcbf2af47881cbbd2c55ba1a671
Catherine Gronkiewicz <35318088+catherinegronkiewicz@users.noreply.github.com>
2020-12-01 15:30:27 -0800

    Update User.java

--- src/miniTwitter/User.java
+++ src/miniTwitter/User.java
@@ -15,93 +15,93 @@ import java.util.HashMap;
 
 public class User implements UserElement, Observer, Subject {
 
-	private String uniqueID;
-	private HashMap<String, User> userMap = AdminPanel.totalUserMap();
-	private ArrayList<String> userFollowers;
-	private ArrayList<String> userFollowings;
-	private ArrayList<String> userFeed;
-	private User lastUpdated;
-	private Long creationTime, lastUpdateTime;
+    private String uniqueID;
+    private HashMap<String, User> userMap = AdminPanel.totalUserMap();
+    private ArrayList<String> userFollowers;
+    private ArrayList<String> userFollowings;
+    private ArrayList<String> userFeed;
+    private User lastUpdated;
+    private Long creationTime, lastUpdateTime;
 
-	public User(String twitterUser) {
-		uniqueID = twitterUser;
-		userFollowings = new ArrayList<String>();
-		userFollowers = new ArrayList<String>();
-		userFeed = new ArrayList<String>();
-	}
+    public User(String twitterUser) {
+	uniqueID = twitterUser;
+	userFollowings = new ArrayList<String>();
+	userFollowers = new ArrayList<String>();
+	userFeed = new ArrayList<String>();
+    }
 	
-	public User getUser() {
-		return User.this;
-	}
+    public User getUser() {
+	return User.this;
+    }
 	
-	public ArrayList<String> getUserFollowers() {
-		return userFollowers;
-	}
+    public ArrayList<String> getUserFollowers() {
+	return userFollowers;
+    }
 
-	public ArrayList<String> getUserFollowings() {
-		return userFollowings;
-	}
+    public ArrayList<String> getUserFollowings() {
+	return userFollowings;
+    }
 
-	public ArrayList<String> getUserFeed() {
-		return userFeed;
-	}
+    public ArrayList<String> getUserFeed() {
+	return userFeed;
+    }
 
-	// add methods - Composite pattern
-	public void addFollowings(String following) {
-		userFollowings.add(following);
-	}
+    // add methods - Composite pattern
+    public void addFollowings(String following) {
+	userFollowings.add(following);
+    }
 
-	// add methods - Composite pattern
-	public void addFollowers(String follower) {
-		userFollowers.add(follower);
-	}
+    // add methods - Composite pattern
+    public void addFollowers(String follower) {
+	userFollowers.add(follower);
+    }
 	
-	public User getLastUser() {
-		return lastUpdated;
-	}
+    public User getLastUser() {
+	return lastUpdated;
+    }
 	
-	public Long getTimeUpdate() {
-		return lastUpdateTime;
-	}
+    public Long getTimeUpdate() {
+	return lastUpdateTime;
+    }
 	
-	@Override
-	public void setCreationTime(Long updateTime) {
-		creationTime = updateTime;
-	}
+    @Override
+    public void setCreationTime(Long updateTime) {
+	creationTime = updateTime;
+    }
 	
-	@Override
-	public Long getCreationTime() {
-		return creationTime;
-	}
+    @Override
+    public Long getCreationTime() {
+	return creationTime;
+    }
 
-	// update followers with new tweets - Observer pattern
-	@Override
-	public void update(String tweet, Long tweetTime) {
-		userFeed.add(tweet);
-		lastUpdateTime = tweetTime;
-	}
+    // update followers with new tweets - Observer pattern
+    @Override
+    public void update(String tweet, Long tweetTime) {
+	userFeed.add(tweet);
+	lastUpdateTime = tweetTime;
+    }
 
-	@Override
-	public String getUserID() {
-		return uniqueID;
-	}
+    @Override
+    public String getUserID() {
+	return uniqueID;
+    }
 
-	@Override
-	public void attach(String userName) {
-		getUser().addFollowings(userName);
-		// grabbing the following and updating followers list
-		userMap.get(userName).addFollowers(getUser().getUserID());
-	}
+    @Override
+    public void attach(String userName) {
+	getUser().addFollowings(userName);
+	// grabbing the following and updating followers list
+	userMap.get(userName).addFollowers(getUser().getUserID());
+    }
 
-	@Override
-	public void notifyObservers(String userTweet) {
-		int followerCount = getUser().getUserFollowers().size();
-		for (int i = 0; i < followerCount; i++) {
-			String twitterFollower = getUser().getUserFollowers().get(i);
-			Long timeStamp = System.currentTimeMillis();
-			// updates each follower with new tweet
-			userMap.get(twitterFollower).update(userTweet, timeStamp);
-		}
+    @Override
+    public void notifyObservers(String userTweet) {
+	int followerCount = getUser().getUserFollowers().size();
+	for (int i = 0; i < followerCount; i++) {
+	    String twitterFollower = getUser().getUserFollowers().get(i);
+	    Long timeStamp = System.currentTimeMillis();
+	    // updates each follower with new tweet
+	    userMap.get(twitterFollower).update(userTweet, timeStamp);
 	}
+    }
 	
 }
