46679db869da665d6ccad9a42c4a42ea6c0ab4a2
Catherine Gronkiewicz <35318088+catherinegronkiewicz@users.noreply.github.com>
2020-12-01 16:38:21 -0800

    Update DisplayVisitor.java

--- src/miniTwitter/DisplayVisitor.java
+++ src/miniTwitter/DisplayVisitor.java
@@ -13,41 +13,37 @@ package miniTwitter;
 
 public class DisplayVisitor implements Visitor {
 	
-	// holds most recent user
-	private static String lastUser;
+    // holds most recent user
+    private static String lastUser;
 	
-	@Override
-	public void visit(TotalUserVisitor utv) {
-		utv.getUserTotal();
-		
-	}
+    @Override
+    public void visit(TotalUserVisitor utv) {
+	utv.getUserTotal();	
+    }
 
-	@Override
-	public void visit(GroupTotalVisitor gtv) {
-		gtv.getGroupTotal();	
-			
-	}
+    @Override
+    public void visit(GroupTotalVisitor gtv) {
+	gtv.getGroupTotal();			
+    }
 
-	@Override
-	public void visit(TotalTweetsVisitor ttv) {
-		ttv.getTotalTweets();
-				
-	}
+    @Override
+    public void visit(TotalTweetsVisitor ttv) {
+	ttv.getTotalTweets();			
+    }
 
-	@Override
-	public void visit(PositivePercentVisitor ppv) {
-		ppv.getPosTotal();
-		
-	}
+    @Override
+    public void visit(PositivePercentVisitor ppv) {
+	ppv.getPosTotal();	
+    }
 
-	@Override
-	public void visit(LastUpdatedVisitor lu) {
-		lastUser = lu.getUserID();
-	}
+    @Override
+    public void visit(LastUpdatedVisitor lu) {
+	lastUser = lu.getUserID();
+    }
 	
-	// gets the most recent user
-	public String getLastUser() {
-		return lastUser;
-	}
+    // gets the most recent user
+    public String getLastUser() {
+	return lastUser;
+    }
 
 }
